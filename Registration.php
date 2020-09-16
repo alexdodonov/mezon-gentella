39086f16fb2fd3f947e04386a0f496c6775810ad
gdever <alexey@dodonov.pro>
2020-09-16 18:54:14 +0300

    feature/Method setErrorMessage was fixed

--- Registration.php
+++ Registration.php
@@ -3,6 +3,7 @@ namespace Mezon\Gentella\Views;
 
 use Mezon\HtmlTemplate\HtmlTemplate;
 use Mezon\Application\ViewStatic;
+use Mezon\Gentella\GentellaTemplate;
 
 /**
  * Class Registration
@@ -31,5 +32,30 @@ class Registration extends ViewStatic
     public function __construct(HtmlTemplate $template)
     {
         parent::__construct($template, 'registration');
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \Mezon\Application\ViewBase::setErrorMessage()
+     */
+    public function setErrorMessage($errorMessage): void
+    {
+        parent::setErrorMessage($errorMessage);
+
+        switch ($errorMessage) {
+            case ('passwords-not-match'):
+                $this->getTemplate()->setPageVar(
+                    'message',
+                    GentellaTemplate::dangerMessageContent('Пароли должны совпадать'));
+                break;
+            case ('user-exists'):
+                $this->getTemplate()->setPageVar(
+                    'message',
+                    GentellaTemplate::dangerMessageContent('Пользователь существует'));
+                break;
+            default:
+                break;
+        }
     }
 }
--- Tests/RegistrationUnitTest.php
+++ Tests/RegistrationUnitTest.php
@@ -1,23 +1,54 @@
 <?php
-
 use Mezon\Gentella\Views\Registration;
 use Mezon\Gentella\GentellaTemplate;
 
 class RegistrationUnitTest extends \PHPUnit\Framework\TestCase
 {
+
+    public function registrationDataProvider(): array
+    {
+        return [
+            [
+                '',
+                function (string $result) {
+                    $this->assertStringContainsString('<h1>Регистрация</h1>', $result);
+                }
+            ],
+            [
+                'passwords-not-match',
+                function (string $result) {
+                    $this->assertStringContainsString('<h1>Регистрация</h1>', $result);
+                    $this->assertStringContainsString('Пароли должны совпадать', $result);
+                    $this->assertStringContainsString('<div class="alert alert-danger', $result);
+                }
+            ],
+            [
+                'user-exists',
+                function (string $result) {
+                    $this->assertStringContainsString('<h1>Регистрация</h1>', $result);
+                    $this->assertStringContainsString('Пользователь существует', $result);
+                    $this->assertStringContainsString('<div class="alert alert-danger', $result);
+                }
+            ]
+        ];
+    }
+
     /**
      * Testing constructor
+     *
+     * @dataProvider registrationDataProvider
      */
-    public function testConstructor() : void
+    public function testConstructor(string $message, callable $assert): void
     {
         // setup
         $template = new GentellaTemplate();
         $view = new Registration($template);
+        $view->setErrorMessage($message);
 
         // test body
         $result = $view->render();
 
         // assertions
-        $this->assertStringContainsString("<h1>Регистрация</h1>", $result);
+        $assert($result);
     }
 }
